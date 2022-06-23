40489b11811e0fc0ab8f7e0eeced0e88ab43d20a
John Holmes <john.holmes@weknowtraining.ca>
2022-06-23 14:50:53 -0600

    more stuff

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,29 @@
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
+import { Recipe } from "./recipe.model";
+
+@Injectable()
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Hot Dog',
+      'This is a hot dog',
+      'https://www.summercity.ca/wp-content/uploads/2016/05/HotDogs.jpg',
+      [
+        new Ingredient('Hot Dog', 2),
+        new Ingredient('Bun', 2)
+      ])
+  ];
+
+  constructor(private slService: ShoppingListService) {}
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
+}
